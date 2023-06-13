d8ccfd13fe7fc4daeba6eedbf0b412a11c0792d7
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-06-13 10:54:37 +0545

    Add handleClick event to update the board on click
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
-const Square = ({ value }) => {
+const Square = ({ value, onSquareClick }) => {
 
-  return <button className="square">{value}</button>;
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default Square
